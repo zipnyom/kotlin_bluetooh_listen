201ab07976fab6ada9e707d62161c21c477b1485
zipnyom <44222965+zipnyom@users.noreply.github.com>
2019-12-13 12:42:52 +0900

    Create MainActivity.kt
    최초 커밋

---
+++ MainActivity.kt
@@ -0,0 +1,30 @@
+val mReceiver = object : BroadcastReceiver() {
+        override fun onReceive(contxt: Context?, intent: Intent?) {
+
+            when (intent?.action) {
+                BluetoothAdapter.ACTION_STATE_CHANGED -> {
+                    val state = intent.getIntExtra(
+                        BluetoothAdapter.EXTRA_STATE,
+                        BluetoothAdapter.ERROR
+                    );
+                    when (state) {
+                        BluetoothAdapter.STATE_OFF -> {
+                                Timber.d("BluetoothAdapter.STATE_OFF ")
+                        }
+                        BluetoothAdapter.STATE_TURNING_OFF -> {
+                            Timber.d("BluetoothAdapter.STATE_TURNING_OFF ")
+                        }
+                        BluetoothAdapter.STATE_ON -> {
+                            beaconInitialize()
+                            Timber.d("BluetoothAdapter.STATE_ON ")
+                        }
+                        BluetoothAdapter.STATE_TURNING_ON -> {
+                            Timber.d("BluetoothAdapter.STATE_TURNING_ON ")
+                        }
+
+                    }
+
+                }
+            }
+        }
+    }
